4e5386080f07c0e47e967c43288a48db1b8106e9
Gishgiish <gishnora@gmail.com>
2024-06-07 12:07:01 +0300

    DigitalClock

---
+++ digital-clock/src/DigitalClock.jsx
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useState, useEffect} from 'react';
+
+function DigitalClock(){
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        },1000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+
+    }, []);
+
+    function formatTime(){
+        let hours = time.getHours();
+        const meridiem = hours >= 12? "PM" : "AM" ;
+        hours = hours % 12 || 12;
+        hours = hours.toString().padStart(2, 0)
+        const minutes = time.getMinutes().toString().padStart(2, 0);
+        const seconds = time.getSeconds().toString().padStart(2, 0);
+
+        return `${hours}:${minutes}:${seconds} ${meridiem}`;
+
+    }
+    
+
+
+    return(
+    <div className='clock-container'>
+        <div className='clock'>
+            <span>{formatTime()}</span>
+        </div>
+    </div>)
+
+}
+
+export default DigitalClock
